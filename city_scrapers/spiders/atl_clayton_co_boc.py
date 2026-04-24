870ca73c26b0ed43bae006688c6d0a5c9b365274
Doina Fitchevici <133082403+DoinaFitchevici@users.noreply.github.com>
2026-04-24 15:06:29 -0400

    Added title, start, links, source, classification

--- city_scrapers/spiders/atl_clayton_co_boc.py
+++ city_scrapers/spiders/atl_clayton_co_boc.py
@@ -1,5 +1,8 @@
 import scrapy
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from dateutil.parser import parse as dateparser
+from city_scrapers_core.constants import BOARD, NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -8,76 +11,64 @@ class AtlClaytonCoBocSpider(CityScrapersSpider):
     name = "atl_clayton_co_boc"
     agency = "Clayton County Board of Commissioners"
     timezone = "America/Chicago"
-    start_urls = ["https://claytoncountyga.primegov.com/public/portal?fromiframe=true"]
-    api_url = "https://claytoncountyga.primegov.com/api/v2/PublicPortal/ListArchivedMeetings?year=2026&_=1776968938107"
+    source_url = "https://claytoncountyga.primegov.com/public/portal?fromiframe=true"
+    api_url = "https://claytoncountyga.primegov.com/api/v2/PublicPortal/ListArchivedMeetings?year={year}"
+    attachment_url = "https://claytoncountyga.primegov.com/Public/CompiledDocument?meetingTemplateId={template_id}&compileOutputType=1"
+    html_url = "https://claytoncountyga.primegov.com/Portal/Meeting?meetingTemplateId={template_id}"
 
     custom_settings = {
         "ROBOTSTXT_OBEY": False,
     }
 
     def start_requests(self):
-        # payload = {"committeeId": self.committee_id}
-        yield scrapy.Request(
-            url=self.api_url,
-            method="GET",
-            # body=json.dumps(payload),
-            # headers={"Content-Type": "application/json"},
-            callback=self.parse,
-        )
-
+        current_year = datetime.now(ZoneInfo(self.timezone)).year
+        for year in range(current_year - 3, current_year + 1):
+            yield scrapy.Request(
+                url=self.api_url.format(year=year),
+                callback=self.parse,
+                )
     def parse(self, response):
+        response = response.json()
         """
         `parse` should always `yield` Meeting items.
 
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        for item in response.css(".meetings"):
+        for item in response:
             meeting = Meeting(
                 title=self._parse_title(item),
-                description=self._parse_description(item),
-                classification=self._parse_classification(item),
+                description="",
+                classification=BOARD,
                 start=self._parse_start(item),
-                end=self._parse_end(item),
-                all_day=self._parse_all_day(item),
-                time_notes=self._parse_time_notes(item),
+                end=None,
+                all_day=False,
+                time_notes="",
                 location=self._parse_location(item),
                 links=self._parse_links(item),
-                source=self._parse_source(response),
+                source=self.source_url,
             )
 
             meeting["status"] = self._get_status(meeting)
             meeting["id"] = self._get_id(meeting)
             yield meeting
-
     def _parse_title(self, item):
         """Parse or generate meeting title."""
 
-        return ""
-
-    def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        return ""
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+        return item.get("title", "")
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        return None
+        date = item.get("date", "")
+        time = item.get("time", "")
+        parsed_start = None
+        if date and time:
+            dt_str = f"{date} {time}"
+            parsed_start = dateparser(dt_str)
+        elif date:
+            parsed_start = dateparser(date)
 
-    def _parse_end(self, item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
-
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
-
-    def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
+        return parsed_start
 
     def _parse_location(self, item):
         """Parse or generate location."""
@@ -88,8 +79,29 @@ class AtlClaytonCoBocSpider(CityScrapersSpider):
 
     def _parse_links(self, item):
         """Parse or generate links."""
-        return [{"href": "", "title": ""}]
+        links = []
 
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
+        parsed_links = item.get("documentList", [])
+        parsed_video_links = item.get("videoUrl", "")
+
+        for link in parsed_links:
+            if "HTML Agenda" in link.get("templateName", ""):
+                href_link = self.html_url.format(template_id=link.get("templateId", "")),
+            else:
+                 href_link = self.attachment_url.format(template_id=link.get("templateId", ""))
+
+            links.append({
+                 "title": link.get("templateName", ""),
+                 "href": href_link,
+               
+            })
+
+        if parsed_video_links:
+
+            links.append({
+                "title": "Video",
+                "href": "https:" + parsed_video_links if not "https:" in parsed_video_links else parsed_video_links,
+                
+            })
+        return links
+
