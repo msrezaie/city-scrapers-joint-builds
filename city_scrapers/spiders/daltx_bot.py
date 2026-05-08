5f761f0a86985aac9ea67a53769d7a6ba26e9d92
Doina Fitchevici <133082403+DoinaFitchevici@users.noreply.github.com>
2026-05-08 08:19:22 -0400

    request video API endpoint and pass its response as meta into primary url request

--- city_scrapers/spiders/daltx_bot.py
+++ city_scrapers/spiders/daltx_bot.py
@@ -1,17 +1,39 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
+import scrapy
 
 
 class DaltxBotSpider(CityScrapersSpider):
     name = "daltx_bot"
     agency = "Dallas College Board of Trustees"
     timezone = "America/Chicago"
-    start_urls = ["https://www.dallascollege.edu/events/?categories%5B%5D=Category%3EBoard%20of%20Trustees&search=all"]
+    start_url = "https://www.dallascollege.edu/events/?categories%5B%5D=Category%3EBoard%20of%20Trustees&search=all"
 
-    videos_endpoint = "https://mediaportal.dallascollege.edu/api/v1.0/live/event?Offset=0&Count=12&Status=Scheduled&Status=Preview"
-
+    videos_endpoint = "https://mediaportal.dallascollege.edu/api/v1.0/media?Count=190&Offset=0&Type=Video&Privacy=Public&Edate={end_date}&Bdate={start_date}"
+    tz=ZoneInfo(timezone)
+    def start_requests(self):
+        start_date = datetime.now(tz=self.tz) - timedelta(days=365 * 3)
+        end_date = datetime.now(tz=self.tz) + timedelta(days=365)
+        
+        yield scrapy.Request(
+            self.videos_endpoint.format(start_date=start_date.strftime("%Y-%m-%d"), end_date=end_date.strftime("%Y-%m-%d")
+            ),
+            callback=self.parse
+        )
     def parse(self, response):
+        videos_meta = response
+        data = response.json()
+        # print(f"DATA: {data}")
+        print(len(data["items"]))
+        yield scrapy.Request(
+            url = self.start_url,
+            meta = {"videos_meta": videos_meta},
+            callback=self.parse_records
+        )
+    def parse_records(self, response):
         records = response.css(".row.calendar-search-results").getall()
         print(len(records))
         # for record in records:
