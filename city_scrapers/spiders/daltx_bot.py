c252ede81b8d0423bb956a65fc68bd14be99f56c
msrezaie <79583632+msrezaie@users.noreply.github.com>
2026-05-11 15:22:10 -0400

    parsed some meeting fields

--- city_scrapers/spiders/daltx_bot.py
+++ city_scrapers/spiders/daltx_bot.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse as date_parser
 import scrapy
 
 
@@ -12,8 +13,11 @@ class DaltxBotSpider(CityScrapersSpider):
     timezone = "America/Chicago"
     start_url = "https://www.dallascollege.edu/events/?categories%5B%5D=Category%3EBoard%20of%20Trustees&search=all"
 
-    videos_endpoint = "https://mediaportal.dallascollege.edu/api/v1.0/media?Count=190&Offset=0&Type=Video&Privacy=Public&Edate={end_date}&Bdate={start_date}"
     tz=ZoneInfo(timezone)
+
+    videos_endpoint = "https://mediaportal.dallascollege.edu/api/v1.0/media?Count=190&Offset=0&Type=Video&Privacy=Public&Edate={end_date}&Bdate={start_date}"
+    video_attachment_url = "https://mediaportal.dallascollege.edu/media/{video_id}/"
+
     def start_requests(self):
         start_date = datetime.now(tz=self.tz) - timedelta(days=365 * 3)
         end_date = datetime.now(tz=self.tz) + timedelta(days=365)
@@ -21,68 +25,73 @@ class DaltxBotSpider(CityScrapersSpider):
         yield scrapy.Request(
             self.videos_endpoint.format(start_date=start_date.strftime("%Y-%m-%d"), end_date=end_date.strftime("%Y-%m-%d")
             ),
-            callback=self.parse
+            callback=self.request_videos
         )
-    def parse(self, response):
-        videos_meta = response
-        data = response.json()
-        # print(f"DATA: {data}")
-        print(len(data["items"]))
+
+    def request_videos(self, response):
+        video_data = response.json()
         yield scrapy.Request(
             url = self.start_url,
-            meta = {"videos_meta": videos_meta},
-            callback=self.parse_records
+            meta = {"videos_meta": video_data},
+            callback=self.parse
         )
-    def parse_records(self, response):
-        records = response.css(".row.calendar-search-results").getall()
-        print(len(records))
-        # for record in records:
-            # print(f"HERE: {record}")
-            # meeting = Meeting(
-            #     title=self._parse_title(item),
-            #     description=self._parse_description(item),
-            #     classification=self._parse_classification(item),
-            #     start=self._parse_start(item),
-            #     end=self._parse_end(item),
-            #     all_day=self._parse_all_day(item),
-            #     time_notes=self._parse_time_notes(item),
-            #     location=self._parse_location(item),
-            #     links=self._parse_links(item),
-            #     source=self._parse_source(response),
-            # )
 
-            # meeting["status"] = self._get_status(meeting)
-            # meeting["id"] = self._get_id(meeting)
+    def parse(self, response):
+        records = response.css(".row.calendar-search-results")
+        video_items = response.meta.get("videos_meta", {}).get("items", [])
 
-            # yield meeting
+        for item in records:
+            detail_url = item.css("h4.cal-header a::attr(href)").get()
+
+            yield response.follow(
+                url=response.urljoin(detail_url),
+                callback=self._construct_meeting,
+                meta={"video_items": video_items, "item": item}
+            )
+
+    def _construct_meeting(self, response):
+        item = response.meta["item"]
+        start, end, all_day = self._parse_datetime(item)
+        meeting = Meeting(
+            title=self._parse_title(item),
+            description=self._parse_description(item),
+            classification=BOARD,
+            start=start,
+            end=end,
+            all_day=all_day,
+            time_notes="",
+            location=self._parse_location(item),
+            links=self._parse_links(item),
+            source=response.url,
+        )
+
+        meeting["status"] = self._get_status(meeting)
+        meeting["id"] = self._get_id(meeting)
+
+        yield meeting
 
     def _parse_title(self, item):
-        """Parse or generate meeting title."""
-        return ""
+        item_str = item.css("h4.cal-header a::text").get()
+        return item_str.strip() if item_str else ""
 
     def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        return ""
+        item_str = item.css(".cal-summary p::text").get()
+        return item_str.strip() if item_str else ""
 
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+    def _parse_datetime(self, item) -> tuple[datetime, datetime, bool]:
+        date_list = item.css(".month::text, .day::text, .year::text").getall()
+        date_str = " ".join(date_list).strip()
 
-    def _parse_start(self, item):
-        """Parse start datetime as a naive datetime object."""
-        return None
+        start_time = item.css(".mb-2 span:nth-child(2)::text").get()
+        end_time = item.css(".mb-2 span:nth-child(3)::text").get()
+        
+        if start_time == "All day":
+            return date_parser(f"{date_str}"), date_parser(f"{date_str}"), True
 
-    def _parse_end(self, item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
+        start_dt = date_parser(f"{date_str} {start_time.strip()}")
+        end_dt = date_parser(f"{date_str} {end_time.strip()}")
 
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
-
-    def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
+        return start_dt, end_dt, False
 
     def _parse_location(self, item):
         """Parse or generate location."""
@@ -94,7 +103,3 @@ class DaltxBotSpider(CityScrapersSpider):
     def _parse_links(self, item):
         """Parse or generate links."""
         return [{"href": "", "title": ""}]
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
